080a633fa6ef739b60d6a5d964a1bf7b5121c003
Walven <walven.killian@gmail.com>
2020-10-27 18:14:00 +0100

    New error message

--- commands/accessSDK.js
+++ commands/accessSDK.js
@@ -29,7 +29,7 @@ module.exports = class accessSDK extends Command {
             action = 'requestAccess';
             // Check if the author is polite
             if ((/([Hh]ello)|([Hh]i)|([Hh]ey)|([Hh]owdy)|([Bb]onjour)|([Bb]onsoir)|([Ss]alut)|([Cc]oucou)/).test(message.content)) {
-                
+
                 // Check if the question is in english
                 if ((/(?=.*(([Mm]ay)|([Cc]an)|([Cc]ould)|([Ww]ould)|([Dd]ownload)))(?=.*((access)|(link)|([Pp][Ss][Dd][Kk])|([Ss][Dd][Kk])))/).test(message.content)) {
                     lang = "en"
@@ -42,54 +42,55 @@ module.exports = class accessSDK extends Command {
                     return true
                 }
             }
-        }
-        else if (message.channel.name.includes('en-')) {
+        } else if (message.channel.name.includes('en-')) {
             lang = 'en';
             action = 'updateRoles';
-        }
-        else if (message.channel.name.includes('fr-')) {
+        } else if (message.channel.name.includes('fr-')) {
             lang = 'fr';
             action = 'updateRoles';
         }
     }
 
     static action(message) {
-        if(action === 'requestAccess') {
+        if (action === 'requestAccess') {
             this.addRole(message);
-        } else if(action === 'updateRoles') {
+        } else if (action === 'updateRoles') {
             this.updateRole(message);
         }
     }
 
     static addRole(message) {
         let author = message.member
-        let answerNb = Math.floor(Math.random()*6)
+        let answerNb = Math.floor(Math.random() * 6)
 
-        if (author.roles.has(SDK_OLD)) {
-            if (lang === "fr") {
-                var answer = "Tu as déjà les accès ! Tu peux télécharger Pokémon SDK dans <#483747311495938058>."
-            }
+        if (author != null && author.roles != null) {
+            if (author.roles.has(SDK_OLD)) {
+                if (lang === "fr") {
+                    var answer = "Tu as déjà les accès ! Tu peux télécharger Pokémon SDK dans <#483747311495938058>."
+                }
 
-            if (lang === "en") {
-                var answer = "You've already got access! You can download Pokémon SDK in <#483747311495938058>."
-            }
-        } 
-        else {
-            message.react("✅")
-            author.addRole(SDK_OLD) // For access
+                if (lang === "en") {
+                    var answer = "You've already got access! You can download Pokémon SDK in <#483747311495938058>."
+                }
+            } else {
+                message.react("✅")
+                author.addRole(SDK_OLD) // For access
 
-            if (lang === "fr") {
-                author.addRole(SDK_FR_ID) // For mention
-                var answer = FR_answers[answerNb] + " Tu peux télécharger Pokémon SDK dans <#483747311495938058>. " + "(Et n'oublie pas de télécharger les ressources dans <#484069458206392331> !)"
-            }
+                if (lang === "fr") {
+                    author.addRole(SDK_FR_ID) // For mention
+                    var answer = FR_answers[answerNb] + " Tu peux télécharger Pokémon SDK dans <#483747311495938058>. " + "(Et n'oublie pas de télécharger les ressources dans <#484069458206392331> !)"
+                }
 
-            if (lang === "en") {
-                author.addRole(SDK_EN_ID) // For mention
-                var answer = EN_answers[answerNb] + " You can download Pokémon SDK in <#483747311495938058>. " + "(Don't forget to download the resources in <#484069458206392331>!)"
+                if (lang === "en") {
+                    author.addRole(SDK_EN_ID) // For mention
+                    var answer = EN_answers[answerNb] + " You can download Pokémon SDK in <#483747311495938058>. " + "(Don't forget to download the resources in <#484069458206392331>!)"
+                }
             }
+            message.channel.send(author + " " + answer);
+        } else {
+            message.channel.send("Something went wrong with the last message")
         }
 
-        message.channel.send(author + " " + answer);
     }
 
     static updateRole(message) {
