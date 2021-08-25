4d7254ecb125afa09781d91d8d76d6bbe4b8ea16
badong <gautier.bardon@gmail.com>
2021-08-25 23:42:25 +0200

    doc command

---
+++ commands/doc.js
@@ -0,0 +1,46 @@
+/**
+ * Sends a button with a link to the documentation
+ */
+const { MessageActionRow, MessageButton } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+// Global config
+const config = require('../config.json');
+
+// Local config
+const commandConfig = {
+	replySentence: {
+		en: 'Here\'s the link to the documentation',
+		fr: 'Voici le lien vers la documentation',
+	},
+	buttonLabel: {
+		en: 'Access the documentation',
+		fr: 'Accéder à la documentation',
+	},
+};
+
+module.exports = {
+	// Build slash command
+	data: new SlashCommandBuilder()
+		.setName('doc')
+		.setDescription('Get link to the documentation')
+		.addStringOption(option =>
+			option.setName('lang')
+				.setDescription('language')
+				.setRequired(false)
+				.addChoice('fr', 'fr')
+				.addChoice('en', 'en')),
+
+	// Command action
+	async execute(interaction) {
+		const lang = interaction.options.getString('lang');
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setStyle('LINK')
+					.setURL(config.link.documentation)
+					.setLabel(commandConfig.buttonLabel[lang]),
+			);
+		await interaction.reply({ content: commandConfig.replySentence[lang], components: [row] });
+	},
+};
--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const { CLIENT_ID, GUILD_ID, TOKEN } = process.env;
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -11,12 +12,12 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '9' }).setToken(TOKEN);
 
 (async () => {
 	try {
 		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
 			{ body: commands },
 		);
 
