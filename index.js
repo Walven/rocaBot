58a3ad7a526ef5388df5cde4e05f2ab6b8b6d04c
badong <gautier.bardon@gmail.com>
2021-08-25 21:30:37 +0200

    basic command handling

--- index.js
+++ index.js
@@ -1,82 +1,35 @@
 const fs = require('fs');
-const Discord = require('discord.js');
-const { prefix, psdkAccessChannelName, channelLog } = require('./config.json')
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-
-    // set a new item in the Collection
-    // with the key as the command name and the value as the exported module
-    client.commands.set(command.name, command);
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
 }
 
 client.once('ready', () => {
-    console.log('rocaBot is ready to manage everything')
-})
-
-client.on('message', message => {
-    if (message.author.bot || message.channel.type === 'dm') return;
-
-    if (message.content.startsWith(prefix)) {
-        const args = message.content.slice(prefix.length).trim().split(/ +/);
-        const commandName = args.shift().toLowerCase();
-
-        if (!client.commands.has(commandName)) return;
-        const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-        if (!command) return message.channel.send('That command do not exist');
-
-        if (command.args && !args.length) {
-            let reply = message.channel.send(`You didn't provide any arguments, ${message.author}`)
-            if (command.usage) {
-                replay += `\nThe proper usage would be: \`${prefix}${command} ${command.usage}\``;
-            }
-
-            return message.channel.send(reply);
-        }
-
-        try {
-            command.execute(message, args);
-        } catch (error) {
-            console.error(error);
-            message.channel.send('There was an error trying to execute that command');
-            sendError(message, error)
-        }
-
-    } else if (message.channel.name === psdkAccessChannelName) {
-        const commandName = "accessSDK";
-        const command = client.commands.get(commandName);
-
-        if (!command) return message.channel.send('accessSDK command file looks missing');
-
-        try {
-            command.execute(message);
-        } catch (error) {
-            console.error(error);
-            message.channel.send('There was an error trying to give access');
-            sendError(message, error)
-        }
-    } else {
-        return;
-    }
+	console.log('Ready!');
 });
 
-function sendError(message, error) {
-    message.member.guild.channels.cache.find(channel => channel.name == channelLog).send(
-        `RocaBot have a problem with "${message.content}" in <#${message.channel.id}> :\n
-        ${error}`
-    )
-}
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
-if (process.env.BOT_TOKEN) {
-    client.login(process.env.BOT_TOKEN);
-} // For local dev
-else {
-    const devConfig = require("./dev-config.json")
-    client.login(devConfig.token);
-}
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	}
+	catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.login(token);
