ff53ed0b5977e6ef9f40c00df29ce63a45cccd49
badong <gautier.bardon@gmail.com>
2021-08-26 20:47:55 +0200

    added some comments

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,19 +1,22 @@
+/**
+ * Run this file to register slash commands to the guild
+ */
 require('dotenv').config();
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { CLIENT_ID, GUILD_ID, TOKEN } = process.env;
 
+// Read command files
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
+// Register the commands
 const rest = new REST({ version: '9' }).setToken(TOKEN);
-
 (async () => {
 	try {
 		await rest.put(
