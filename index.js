69b0d5461fcabb4576e126c7c1bd4cd4979520d3
Aerun <nicolasdoll54@gmail.com>
2021-08-30 19:49:38 +0200

    Improve & clean code

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ client.once('ready', () => {
 
 // COMMANDS
 
-const usersSpamMap = new Map();
-const usersSpamContentMap = new Map();
+// const usersSpamMap = new Map();
+// const usersSpamContentMap = new Map();
 
 client.on('message', message => {
     if (message.author.bot || message.channel.type === 'dm') return;
@@ -72,6 +72,7 @@ client.on('message', message => {
             sendError(message, error)
         }
     } else {
+/*
         if (usersSpamMap.has(message.author.id)) {
             const messageSpamLimit = 6
             const messageSpamLimitKick = 9
@@ -107,6 +108,7 @@ client.on('message', message => {
                 usersSpamMap.delete(message.author.id);
             }, 10000);
         }
+*/
         return;
     }
 });
@@ -119,26 +121,25 @@ client.on('guildMemberAdd', member => {
 
     const channelId = '143824995867557888';
     const channelRules = '360855474284789772';
-    const messageFr = `:french_bread: Bienvenue sur le serveur de la communauté **Pokémon Workshop**, <@${member.id}> !\n\nIci, tu trouveras de quoi **Télécharger et apprendre à utiliser PokémonSDK**, pour créer ton propre **fangame Pokémon**.\nTu pourras aussi discuter du logiciel et rencontrer toute une communauté de fans de Pokémon et de création !\n\n:arrow_forward: Pour accéder au reste du serveur et à la sélection des rôles, lis les <#${channelRules}> , ajoute un :thumbsup: et on est parti ! :arrow_backward:`;
-    const messageEn = `:globe_with_meridians: Welcome on the **Pokémon Workshop** server, <@${member.id}> !\n\nHere, you'll be able to **Download & Learn to use PokémonSDK**, in order to create your own **Pokémon fangame**.\nYou'll also find channels to talk about the software, but also with a big Pokémon & Makers community!\n\n:arrow_forward: To access the rest of the server and the role selection, please make sure to read the <#${channelRules}> , add a :thumbsup: and off we go! :arrow_backward:`;
+    const messageFr = `:french_bread: Bienvenue sur le serveur de la communauté **Pokémon Workshop**, <@{userId}> !\n\nIci, tu trouveras de quoi **Télécharger et apprendre à utiliser PokémonSDK**, pour créer ton propre **fangame Pokémon**.\nTu pourras aussi discuter du logiciel et rencontrer toute une communauté de fans de Pokémon et de création !\n\n:arrow_forward: Pour accéder au reste du serveur et à la sélection des rôles, lis les <#{channelRules}>, ajoute un :thumbsup: et on est parti ! :arrow_backward:`;
+    const messageEn = `:globe_with_meridians: Welcome on the **Pokémon Workshop** server, <@{userId}>!\n\nHere, you'll be able to **Download & Learn to use PokémonSDK**, in order to create your own **Pokémon fangame**.\nYou'll also find channels to talk about the software, but also with a big Pokémon & Makers community!\n\n:arrow_forward: To access the rest of the server and the role selection, please make sure to read the <#{channelRules}>, add a :thumbsup: and off we go! :arrow_backward:`;
     if (member.user.locale === 'fr') {
-        member.guild.channels.cache.get(channelId).send(messageFr);
+        member.guild.channels.cache.get(channelId).send(messageFr.replace('{userId}', member.id.toString()).replace('{channelRules}', channelRules.toString()));
     } else {
-        member.guild.channels.cache.get(channelId).send(messageEn);
+        member.guild.channels.cache.get(channelId).send(messageEn.replace('{userId}', member.id.toString()).replace('{channelRules}', channelRules.toString()));
     }
     console.log(`${member.user.username} joined the Pokémon Workshop server!`)
 });
 
 // Add a role when a member reacts to a message
+isReactionEventValid = (reaction, user) => (!user.bot && reaction.message.guild);
 client.on('messageReactionAdd', (reaction, user) => {
-    if (user.bot) return;
+    if (!isReactionEventValid) return;
     console.log(`Reaction added by ${user.username}. Emoji name: ${reaction.emoji.name}.`);
 
-    if (!reaction.message.guild) return;
     const reactionRoleElem = config.reactionRole[reaction.message.id];
     if (!reactionRoleElem) return;
-    const prop = reaction.emoji.id ? 'id' : 'name';
-    const emoji = reactionRoleElem.emojis.find(emoji => emoji[prop] === reaction.emoji[prop]);
+    const emoji = reactionRoleElem.emojis.find(emoji => emoji.name === reaction.emoji.name);
     if (emoji) {
         console.log(`Add the ${emoji.roles} role to ${user.username}.`);
         reaction.message.guild.member(user).roles.add(emoji.roles);
@@ -147,14 +148,12 @@ client.on('messageReactionAdd', (reaction, user) => {
 
 // Remove a role when a member reacts to a message
 client.on('messageReactionRemove', (reaction, user) => {
-    if (user.bot) return;
+    if (!isReactionEventValid) return;
     console.log(`Reaction removed by ${user.username}. Emoji name: ${reaction.emoji.name}.`);
 
-    if (!reaction.message.guild) return;
     const reactionRoleElem = config.reactionRole[reaction.message.id];
     if (!reactionRoleElem || !reactionRoleElem.removable) return;
-    const prop = reaction.emoji.id ? 'id' : 'name';
-    const emoji = reactionRoleElem.emojis.find(emoji => emoji[prop] === reaction.emoji[prop]);
+    const emoji = reactionRoleElem.emojis.find(emoji => emoji.name === reaction.emoji.name);
     if (emoji) {
         console.log(`Remove the ${emoji.roles} role to ${user.username}.`);
         reaction.message.guild.member(user).roles.remove(emoji.roles);
