0fd57302d3c23dee1c0b20e581c286850fe7fe99
Badong <gautier.bardon@gmail.com>
2022-04-30 19:56:53 +0200

    wip algorithm & set base tld whitelist

--- src/events/message.js
+++ src/events/message.js
@@ -1,6 +1,7 @@
 const { bold, userMention, channelMention } = require('@discordjs/builders');
 const config = require (__dirname + '/../../config.json');
 const { readFileSync } = require('fs');
+const os = require('os');
 
 const NO_KICK_TLDS = ['fr', 'com', 'net', 'eu', 'info', 'org',];
 //stolen from https://regexr.com/2rj36
@@ -12,10 +13,32 @@ module.exports = {
 	execute: (client, message) => {
         console.log(message.content);
         //Get message urls
-        let urls = message.content.match(URL_REGEX);
+        let messageUrls = message.content.match(URL_REGEX);
         
-        if (!urls) {
+        if (!messageUrls) {
             return;
         }
+
+        const allowedUrls = readFileSync(__dirname + '/message/url_whitelist.txt', 'utf-8').split(os.EOL);
+        const deniedMessageUrls = messageUrls.filter(messageUrl => !allowedUrls.includes(messageUrl));
+
+        if (!deniedMessageUrls) {
+            return;
+        }
+
+        const allowedTlds = readFileSync(__dirname + '/message/tld_whitelist.txt', 'utf-8').split(os.EOL);
+        const deniedMessageTlds = messageUrls.filter(messageUrl => !allowedTlds.includes(messageUrl.split('.').pop()));
+
+        console.log(deniedMessageTlds);
+
+        if (deniedMessageTlds) {
+            //kick person
+        }
+
+        if (deniedMessageUrls) {
+            //remove message
+        }
+
+        
 	},
 };
