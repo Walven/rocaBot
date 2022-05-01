a14214828048b77ce2dfe269fd4cc35bad36f243
Badong <gautier.bardon@gmail.com>
2022-05-01 13:46:38 +0200

    working remove/kick

--- src/events/message.js
+++ src/events/message.js
@@ -1,44 +1,37 @@
-const { bold, userMention, channelMention } = require('@discordjs/builders');
-const config = require (__dirname + '/../../config.json');
 const { readFileSync } = require('fs');
 const os = require('os');
 
-const NO_KICK_TLDS = ['fr', 'com', 'net', 'eu', 'info', 'org',];
 //stolen from https://regexr.com/2rj36
 const URL_REGEX = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
 
 module.exports = {
 	name: 'messageCreate',
 	once: false,
-	execute: (client, message) => {
-        console.log(message.content);
-        //Get message urls
-        let messageUrls = message.content.match(URL_REGEX);
-        
-        if (!messageUrls) {
-            return;
-        }
+	execute: (client, message) => {        
+        //Check for unkickable roles
+        const userRoles = message.member.roles.cache;
+        let userIsStaff = false;
+        userRoles.forEach(role => {
+            if (role.name === 'staff') {
+                userIsStaff = true;
+            }
+        });
+         if (userIsStaff) return;
 
+        let messageUrls = message.content.match(URL_REGEX);
+        if (!messageUrls) return;
+        
         const allowedUrls = readFileSync(__dirname + '/message/url_whitelist.txt', 'utf-8').split(os.EOL);
         const deniedMessageUrls = messageUrls.filter(messageUrl => !allowedUrls.includes(messageUrl));
-
-        if (!deniedMessageUrls) {
-            return;
-        }
-
+    
         const allowedTlds = readFileSync(__dirname + '/message/tld_whitelist.txt', 'utf-8').split(os.EOL);
         const deniedMessageTlds = messageUrls.filter(messageUrl => !allowedTlds.includes(messageUrl.split('.').pop()));
 
-        console.log(deniedMessageTlds);
-
-        if (deniedMessageTlds) {
-            //kick person
+        if (deniedMessageTlds.length) {
+            message.member.kick(`Posted link with invalid tld ${deniedMessageTlds.join(', ')}`);
         }
-
-        if (deniedMessageUrls) {
-            //remove message
+        if (deniedMessageUrls.length) {
+            message.delete().catch(console.error);
         }
-
-        
 	},
 };
